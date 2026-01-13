8bf0da17737512b661523e75b837e52a70b85846
rmartinez_8739 <rmartinez_8739@southtexascollege.edu>
2026-01-13 08:23:17 -0600

    updated fees_fines sql

--- queries/lib_fees_fines.sql
+++ queries/lib_fees_fines.sql
@@ -1,8 +1,8 @@
---metadb:function lib_banner_holds
+--metadb:function lib_fees_fines
 
-DROP FUNCTION IF EXISTS lib_banner_holds;
+DROP FUNCTION IF EXISTS lib_fees_fines;
 
-CREATE FUNCTION lib_banner_holds(
+CREATE FUNCTION lib_fees_fines(
     min_fee numeric DEFAULT 400.00,
     max_fee numeric DEFAULT 1000000.00
 )
@@ -13,8 +13,9 @@ RETURNS TABLE(
     d_username text,
     e_last_name text,
     f_first_name text,
-    g_item_title text,
-    h_fee_balance text
+    g_barcode text,
+    h_item_title text,
+    i_fee_balance text
 )
 AS $$
 SELECT
@@ -23,19 +24,22 @@ SELECT
     jsonb_extract_path_text(g.jsonb, 'group') AS c_patron_profile,
     jsonb_extract_path_text(u.jsonb, 'username') as d_username ,
     jsonb_extract_path_text(u.jsonb, 'personal', 'lastName') AS e_last_name ,
-    jsonb_extract_path_text(u.jsonb, 'personal', 'firstName') AS f_first_name ,    
-    jsonb_extract_path_text(a.jsonb, 'title') AS g_item_title,
-    jsonb_extract_path_text(a.jsonb, 'remaining') AS h_fee_balance
+    jsonb_extract_path_text(u.jsonb, 'personal', 'firstName') AS f_first_name ,
+    jsonb_extract_path_text(i.jsonb, 'barcode') as g_barcode,
+    jsonb_extract_path_text(a.jsonb, 'title') AS h_item_title,
+    jsonb_extract_path_text(a.jsonb, 'remaining') AS i_fee_balance
 FROM
     folio_feesfines.accounts AS a
     LEFT JOIN folio_users.users AS u
     ON u.id = jsonb_extract_path_text(a.jsonb, 'userId')::uuid
     LEFT JOIN folio_users.groups AS g
     ON g.id = jsonb_extract_path_text(u.jsonb, 'patronGroup')::uuid
+    LEFT JOIN folio_inventory.item i 
+    ON i.id = jsonb_extract_path_text(a.jsonb, 'itemId')::uuid
 WHERE
     (jsonb_extract_path_text(a.jsonb, 'remaining')::numeric >= min_fee AND jsonb_extract_path_text(a.jsonb, 'remaining')::numeric <= max_fee)
 ORDER BY
-    a_fee_date DESC, d_username ASC, h_fee_balance DESC
+    a_fee_date DESC, d_username ASC, i_fee_balance DESC
 $$
 LANGUAGE SQL
 STABLE
