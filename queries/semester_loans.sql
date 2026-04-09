9b0b701ca447c836e6af3db202ef57193dd54275
mresen19 <mresen19@southtexascollege.edu>
2026-04-09 14:59:51 -0500

    Updated semester_loans.sql

--- queries/semester_loans.sql
+++ queries/semester_loans.sql
@@ -20,7 +20,7 @@ RETURNS TABLE(
     "K - Staff Notes" TEXT
 )
 AS $$
-    WITH loans AS (
+    WITH loans AS MATERIALIZED (
         SELECT
             it.id AS item_id,
             jsonb_extract_path_text(l.jsonb, 'dueDate') AS due_date,
@@ -28,15 +28,15 @@ AS $$
             NULLIF(CONCAT(jsonb_extract_path_text(u.jsonb, 'personal', 'firstName'), ' ', jsonb_extract_path_text(u.jsonb, 'personal', 'lastName')), ' ') AS full_name,
             jsonb_extract_path_text(u.jsonb, 'personal', 'phone') AS phone,
             jsonb_extract_path_text(u.jsonb, 'personal', 'email') AS email,
-		    jsonb_extract_path_text(lc.jsonb, 'name') AS checkout_campus
-		FROM folio_inventory.instance ins
-		JOIN folio_inventory.holdings_record hr ON hr.instanceid = ins.id
-		JOIN folio_inventory.item it ON it.holdingsrecordid = hr.id
-		JOIN folio_circulation.loan l ON jsonb_extract_path_text(l.jsonb, 'itemId')::uuid = it.id
-		JOIN folio_users.users u ON u.id = jsonb_extract_path_text(l.jsonb, 'userId')::uuid
-		JOIN folio_inventory.location ll ON ll.id = jsonb_extract_path_text(l.jsonb, 'itemEffectiveLocationIdAtCheckOut')::uuid
-		join folio_inventory.loccampus lc on lc.id = jsonb_extract_path_text(ll.jsonb, 'campusId')::uuid
-		WHERE jsonb_extract_path_text(l.jsonb, 'status', 'name') = 'Open'
+            jsonb_extract_path_text(lc.jsonb, 'name') AS checkout_campus
+        FROM folio_inventory.instance ins
+        JOIN folio_inventory.holdings_record hr ON hr.instanceid = ins.id
+        JOIN folio_inventory.item it ON it.holdingsrecordid = hr.id
+        JOIN folio_circulation.loan l ON jsonb_extract_path_text(l.jsonb, 'itemId')::uuid = it.id
+        JOIN folio_users.users u ON u.id = jsonb_extract_path_text(l.jsonb, 'userId')::uuid
+        JOIN folio_inventory.location ll ON ll.id = jsonb_extract_path_text(l.jsonb, 'itemEffectiveLocationIdAtCheckOut')::uuid
+        JOIN folio_inventory.loccampus lc on lc.id = jsonb_extract_path_text(ll.jsonb, 'campusId')::uuid
+        WHERE jsonb_extract_path_text(l.jsonb, 'status', 'name') = 'Open'
     )
     SELECT
         insc.name AS "Subtype",
@@ -54,20 +54,22 @@ AS $$
     JOIN folio_inventory.holdings_record hr ON hr.instanceid = ins.id
     JOIN folio_inventory.item it ON it.holdingsrecordid = hr.id
     JOIN folio_inventory.location__t hl ON hl.id = hr.permanentlocationid
-    join folio_inventory.loclibrary__t ll ON ll.id = hl.library_id
+    JOIN folio_inventory.loclibrary__t ll ON ll.id = hl.library_id
     JOIN folio_inventory.statistical_code__t insc ON insc.id = (jsonb_path_query_first(ins.jsonb, '$.statisticalCodeIds[*]') #>> '{}')::uuid
     JOIN folio_inventory.material_type__t m ON m.id = jsonb_extract_path_text(it.jsonb, 'materialTypeId')::uuid
     LEFT JOIN loans ON loans.item_id = it.id
     WHERE
         (item_library = 'All' OR ll.name = item_library)       
         AND CASE
-            WHEN subtype = 'All' 
-                THEN (insc.name = 'Calculator' AND m.name = 'SEM-ITEM') 
-                OR (insc.name IN ('Laptop', 'Hotspot') AND m.name = 'SEMEXTEND-ITEM')
             WHEN subtype = 'Calculator' 
-                THEN insc.name = subtype AND m.name = 'SEM-ITEM'
-            ELSE 
-                insc.name = subtype AND m.name = 'SEMEXTEND-ITEM'
+                THEN insc.name = 'Calculator' AND m.name = 'SEM-ITEM'
+            WHEN subtype = 'Hotspot' 
+                THEN insc.name = 'Hotspot' AND m.name = 'SEMEXTEND-ITEM' AND hl.name != 'Storage'
+            WHEN subtype = 'Laptop' 
+                THEN insc.name = 'Laptop' AND m.name = 'SEMEXTEND-ITEM'
+            ELSE
+                (insc.name = 'Calculator' AND m.name = 'SEM-ITEM') 
+                OR (insc.name IN ('Laptop', 'Hotspot') AND m.name = 'SEMEXTEND-ITEM')
         END
     ORDER BY
         jsonb_extract_path_text(it.jsonb, 'barcode')
